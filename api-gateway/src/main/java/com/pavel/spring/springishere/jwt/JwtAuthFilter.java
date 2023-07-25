86b1062c21e573cc0e839221cdcce8f58011162e
Pavel Nenov <pavel.nenov@r3.com>
2023-07-25 10:29:02 +0300

    Implemented pass through for authentication requests in api gateway

--- api-gateway/src/main/java/com/pavel/spring/springishere/jwt/JwtAuthFilter.java
+++ api-gateway/src/main/java/com/pavel/spring/springishere/jwt/JwtAuthFilter.java
@@ -3,19 +3,21 @@ package com.pavel.spring.springishere.jwt;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Component
 public class JwtAuthFilter implements GlobalFilter {
 
-    private static final String[] AUTH_WHITELIST = {
+    private static final List<String> SWAGGER_WHITELIST = List.of(
             // -- Swagger UI v2
             "/v2/api-docs",
             "/app/v2/api-docs",
@@ -34,24 +36,26 @@ public class JwtAuthFilter implements GlobalFilter {
 
             //heartbeat
             "/heartbeat"
-    };
+    );
+
+    private static final String AUTH_ENDPOINT = "/app/api/v1/auth/authenticate";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         // do not check auth for auth requests
         String path = exchange.getRequest().getPath().value();
-        for (String s : AUTH_WHITELIST) {
-            if (s.equals(path)) {
-                return chain.filter(exchange);
-            }
+        if (SWAGGER_WHITELIST.contains(path)) {
+            return chain.filter(exchange);
         }
-//        if ("/app/api/v1/auth/**".equals(path)) {
-//            return chain.filter(exchange);
-//        }
 
-        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        //pass through auth requests
+        if (path.equals(AUTH_ENDPOINT)) {
+            ServerWebExchange build = exchange.mutate().request(r -> r.headers(h -> h.remove(AUTHORIZATION))).build();
+            return chain.filter(build);
+        }
 
+        String authHeader = exchange.getRequest().getHeaders().getFirst(AUTHORIZATION);
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String jwtToken = authHeader.substring(7);
             String username = JwtUtil.extractUsername(jwtToken);
